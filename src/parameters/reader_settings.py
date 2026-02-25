37acf324dc8c66dc08fcb1e07b83a24a00572a0a
Luis López <lucho2143103@gmail.com>
2026-02-25 09:52:52 -0500

    feat: Se agrega un archivo con la parametrización requerida para abrir los archivos con pandas

---
+++ src/parameters/reader_settings.py
@@ -0,0 +1,22 @@
+FILES_CONFIG = {
+    "accion-grupo": {
+        "type": "excel",
+        "params": {}
+    },
+    "fallos-favor": {
+        "type": "excel",
+        "params": {"sheet_name": "Detalle"}
+    },
+    "pensionados_fec_status": {
+        "type": "csv",
+        "params": {"sep": ";", "encoding": "latin1"}
+    },
+    "pensionados_fec_vin": {
+        "type": "excel",
+        "params": {}
+    },
+    "pension_gracia": {
+        "type": "excel",
+        "params": {"skiprows": 1}
+    }
+}
